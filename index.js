26466d0cdd756348b762a6e792193714a2e0217d
ImpregnableProgrammer <kapur2001@gmail.com>
2018-02-15 02:07:58 -0800

    Completed Slideshow functionality; Now we just need to add onto it

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+'use strict';
+
 // Slideshow
+var Slides = document.getElementsByClassName("fade");
+var Buttons = document.getElementsByClassName("Button");
+var q = -1;
+var T;
+
+function showSlide(n) {
+    for(var i = 0;i < Slides.length;i++) {
+	Slides[i].style.display = "none";
+	Buttons[i].style.opacity = ".5";
+    }
+    q = (Slides.length + n) % Slides.length;
+    Slides[q].style.display = "block";
+    Buttons[q].style.opacity = "1";
+}
+
+function showSlides() {
+    showSlide(q+1);
+    T = setTimeout(showSlides,10000);
+}
+
+function buttonPress(n) {
+    showSlide(n);
+    clearTimeout(T);
+    T = setTimeout(showSlides,10000);
+}
+
+showSlides();
