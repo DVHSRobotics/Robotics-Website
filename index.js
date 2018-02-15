d4e40a79e5a1c5be5600f82aae77149d368e48ef
ImpregnableProgrammer <kapur2001@gmail.com>
2018-02-15 00:46:00 -0800

    Slideshow work

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-'use strict';
-
-// Slideshow functionality
-var Slides = document.getElementsByClassName("fade")
-var q = 0;
-showSlides();
-function showSlides() {
-    for(var i=0;i<Slides.length;i++) {
-	Slides[i].style.display = "none";
-    }
-    Slides[q % Slides.length].style.display = "block";
-    q++;
-    setTimeout(showSlides,10000);
-}
+// Slideshow
