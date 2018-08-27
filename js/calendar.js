8bc0c8e2d5f6be46220b02ceb91816eb76ca8f4f
rameezhm <rmughal246@yahoo.com>
2018-08-26 22:58:40 -0700

    autogenerate calendar skeleton based on date

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,53 @@
+var currentDateData = new Date();
+var date = currentDateData.getDate();
+var month = currentDateData.getMonth() + 1; //months are counted starting at 0 in JS
+var year = currentDateData.getFullYear();
+var dayOfWeek = currentDateData.getDay();
+
+//initializes calendar with current data populates calendar 
+function calendarInit() {
+    var sundayDate = date;
+    for (var i = dayOfWeek; i > 0; i--) { //calculate the date of Sunday of the current week
+        sundayDate--;
+    }
+    generateCalendar(sundayDate);
+
+}
+
+//generates each of the boxes for each day of the week
+function generateCalendar(sundayDate) {
+    //generate boxes for each day
+    for (var i = 0; i < 7; i++) {
+        boxDate = parseInt(sundayDate + i);
+        if (boxDate > maxDaysOfMonth(month)) {
+            boxDate -= maxDaysOfMonth(month);
+        }
+        document.getElementById('week').innerHTML += `
+        <div class="col day-box" id="mon">
+            <p>Lorem ipsum dolor sit amet,` + boxDate + ` </p>
+        </div>
+        `;
+    }
+}
+
+function maxDaysOfMonth(monthToCheck) {
+    if ((monthToCheck <= 7 && monthToCheck%2===1) || (monthToCheck >= 8 && monthToCheck%2===0)) {
+        return 31;
+    }else if(monthToCheck === 2) {//special condition for February
+        //check for leap year
+        if(year%400 === 0){
+            return 27;
+        }else if(year%4 === 0 && year%100 != 0) {
+            return 28;
+        }else {
+            return 27;
+        }
+    }else {
+        return 30;
+    }
+}
+
+calendarInit();
 
 //Changes calendar display to next week
 function nextWeek() {
